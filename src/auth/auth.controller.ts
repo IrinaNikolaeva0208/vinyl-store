d56cdd5cffe0745e6a4be4814d72066776fd91dc
Irina Nikolaeva <irina2002.nikolaeva@yandex.ru>
2024-04-14 19:50:19 +0300

    feat: implement logout endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,6 +4,10 @@ import { Request, Response } from 'express';
 import { AuthService } from './auth.service';
 import { User } from './entities';
 import { Public } from 'src/utils/public.decorator';
+import {
+  AUTH_REDIRECT_ROUTE,
+  LOGOUT_REDIRECT_ROUTE,
+} from 'src/utils/constants';
 
 @Public()
 @Controller('auth')
@@ -26,10 +30,10 @@ export class AuthController {
 
     const { accessToken, refreshToken } =
       await this.authService.getFreshTokens(user);
-    response.cookie('access', accessToken, { httpOnly: true });
-    response.cookie('refresh', refreshToken, { httpOnly: true });
-
-    return { message: 'Successfully logged in' };
+    response
+      .cookie('access', accessToken, { httpOnly: true })
+      .cookie('refresh', refreshToken, { httpOnly: true })
+      .redirect(AUTH_REDIRECT_ROUTE);
   }
 
   @UseGuards(JwtRefreshGuard)
@@ -41,8 +45,17 @@ export class AuthController {
     const { accessToken } = await this.authService.signAccessToken(
       request.user as User,
     );
-    response.cookie('access', accessToken, { httpOnly: true });
 
-    return { message: 'Successfully refreshed' };
+    response
+      .cookie('access', accessToken, { httpOnly: true })
+      .redirect(AUTH_REDIRECT_ROUTE);
+  }
+
+  @Get('logout')
+  logout(@Res() response: Response) {
+    response
+      .clearCookie('access')
+      .clearCookie('refresh')
+      .redirect(LOGOUT_REDIRECT_ROUTE);
   }
 }
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,5 +3,13 @@ const NO_PHOTO_URL =
 
 const FILE_TYPE = /(jpg|jpeg|png|webp)$/;
 const MAX_FILE_SIZE = 10e6;
+const AUTH_REDIRECT_ROUTE = 'vinyl?offset=0&limit=10';
+const LOGOUT_REDIRECT_ROUTE = '/auth/google';
 
-export { FILE_TYPE, MAX_FILE_SIZE, NO_PHOTO_URL };
+export {
+  FILE_TYPE,
+  MAX_FILE_SIZE,
+  NO_PHOTO_URL,
+  AUTH_REDIRECT_ROUTE,
+  LOGOUT_REDIRECT_ROUTE,
+};
