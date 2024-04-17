a324408ff17e896b1d1cea68bfd904a18239e921
Irina Nikolaeva <irina2002.nikolaeva@yandex.ru>
2024-04-17 22:02:42 +0300

    fix: delete barrels from modules to avoid circular dependency

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { VinylModule } from './vinyl';
-import { AuthModule } from './auth';
+import { VinylModule } from './vinyl/vinyl.module';
+import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
