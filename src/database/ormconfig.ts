38c95547f2e1d56752427d1c5f0c635109652b41
Irina Nikolaeva <irina2002.nikolaeva@yandex.ru>
2024-04-20 14:54:50 +0300

    feat: add initial data migration

---
+++ src/database/ormconfig.ts
@@ -0,0 +1,21 @@
+import { DataSourceOptions, DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const options: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.PG_HOST,
+  username: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  database: process.env.PG_DATABASE,
+  entities: ['dist/**/*.entity.js'],
+  migrations: ['dist/database/migrations/*{.ts,.js}'],
+  migrationsRun: true,
+  ssl: true,
+  synchronize: false,
+};
+
+const dataSource = new DataSource(options);
+export { options };
+export default dataSource;
